0c0c616f9ca143d4a4e59c8cd04b18e117a24d3e
smllns <marysmoly@gmail.com>
2024-11-03 15:22:28 +0200

    Добавил поддержку темной и светлой темы в сайдбаре

--- src/components/app-sidebar.tsx
+++ src/components/app-sidebar.tsx
@@ -90,7 +90,8 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
                   <SidebarMenuSub className='ml-0 border-l-0 px-1.5'>
                     {item.items.map((item) => (
                       <SidebarMenuSubItem key={item.title}>
-                        <SidebarMenuSubButton asChild isActive={item.isActive}>
+                        {/* <SidebarMenuSubButton asChild isActive={item.isActive}> */}
+                        <SidebarMenuSubButton asChild>
                           <a href={item.url}>{item.title}</a>
                         </SidebarMenuSubButton>
                       </SidebarMenuSubItem>
